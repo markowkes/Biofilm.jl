01cc43074ca6c6b097d9a46439dee226f54c07f1
Mark Owkes <mark.owkes@montana.edu>
2022-09-19 14:16:13 -0400

    Cleaning up Case 6

--- examples/Case6_threeSpecies.jl
+++ examples/Case6_threeSpecies.jl
@@ -4,15 +4,19 @@ using Biofilm
 KmB1 = 200; KmB3 = 11;  KmC2 = 20;  KI = 1;
 mumaxA = 0.4;  mumaxB = 0.672;  mumaxC = 1.46;
 
-# Define light as a function of time and depth within biofilm
-diss=2000;  # Dissipation rate into biofilm [1/m]
+## Define light as a function of time and depth within biofilm
+
 smoothHeaviside(t,t0)=tanh.(100*(t.-t0).-0.5)
+
 # Light :         turns on at t=0.25             turns off at t=0.75
 intensity(t) = smoothHeaviside(mod(t,1),0.25)-smoothHeaviside(mod(t,1),0.75)
-# Dissipation of light into biofilm (1 at top with a rate of decrease of diss)
-dissipation(z,Lf) = max.(0.0,1.0.-(Lf.-z)*diss)
-light(t,z,Lf) = intensity(t)*dissipation(z,Lf)
 
+# Dissipation of light into biofilm (1 at top with a rate of decrease of diss)
+diss=2000;  # Dissipation rate into biofilm [1/m]
+dissipation(z,Lf) = max.(0.0,1.0.-(Lf.-z)*diss)
+
+# Light at a given location 
+light(t,z,Lf) = intensity(t)*dissipation(z,Lf)
 
 # Define a structure to hold all the parameters
 p = param(
@@ -33,8 +37,9 @@ p = param(
          (t) -> 0.0],
 
     # Time
-    tFinal=1200,   # Simulation time [days]
-    outPeriod=1,  # Time between outputs [days]
+    tFinal=500,   # Simulation time [days]
+    outPeriod=0.25,  # Time between outputs [days]
+    plotPeriod=5,    # Time between plots [days] (make multiple of outPeriod!)
 
     # Simulation
     Title="Ramsing et al. 1993 Test Case",
@@ -59,10 +64,10 @@ p = param(
 
     # Substance Constants
     # Biomass yield coeffficient on substrate
-    #     oxygen  sulfate  sulfide
-    Yxs=[ -0.52    0.0      0.0      # Phototropho produces oxygen
-           0.58    0.0      0.09     # SOB uses oxygen and sulfide
-           0.00    0.584   -1.645],  # SRB uses sulfate and produces sulfide
+    #     oxygen  sulfate  Hy. sulfide
+    Yxs=[ -0.52    0.0      0.0        # Phototropho produces oxygen
+           0.58    0.0      0.09       # SOB uses oxygen and sulfide
+           0.00    0.584   -1.645],    # SRB uses sulfate and produces sulfide
     Daq=[1.51e-4,8e-5,1.21e-4],    # Substrate diffusion through boundary layer
     De =[6.8e-5,4e-5,6.04e-5],     # Substrate diffusion through biofilm     
     rho=[2.5e5,2.5e5,2.5e5],     # Particulate densities
@@ -74,3 +79,22 @@ p = param(
 
 t,zm,X,S,Pb,Sb,Lf,sol = BiofilmSolver(p) # Run solver
 makePlots(t,zm,X,S,Pb,Sb,Lf,p) # Plot final results
+
+# Post processing
+# Call movieBiofilm() to make a movie of the biofilm particulates and substrates
+using Plots
+function movieBiofilm()
+    
+    times=1:50 ### Adjust the times as needed ####
+
+    # Make animation
+    anim = @animate for t in times 
+        analyzeBiofilm(sol,p,t,makePlot=true)
+    end
+
+    # View annimation 
+    gif(anim) # Make a gif
+    #gif(anim,"anim.mp4")  # Make a .mp4
+
+end
+#movieBiofilm()
