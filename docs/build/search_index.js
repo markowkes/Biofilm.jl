e1024715cb0470516d3ad2ffab7fe26bb3ceb115
markowkes <mark.owkes@montana.edu>
2022-09-20 11:11:22 -0400

    Testing adding build folder

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"postprocessing/#Postprocessing","page":"-","title":"Postprocessing","text":"","category":"section"},{"location":"postprocessing/","page":"-","title":"-","text":"analyzeBiofilm","category":"page"},{"location":"postprocessing/#Biofilm.analyzeBiofilm","page":"-","title":"Biofilm.analyzeBiofilm","text":"analyzeBiofilm(sol,p,t)\nanalyzeBiofilm(sol,p,t,makePlot=true)\n\nTake solution from biofilm solver and outputs variabes and a plot of biofilm variables.\n\n\n\n\n\n","category":"function"},{"location":"","page":"-","title":"-","text":"This package models the dynamics of a biofilm.  ","category":"page"},{"location":"","page":"-","title":"-","text":"","category":"page"}]
+}
