5ef2223a943f74b3be579290e3a3b2c6ed926ff5
markowkes <mark.owkes@montana.edu>
2022-09-20 14:05:12 -0400

    Update to Cases 6 & 7
    Bug in growthrates and various parameter changes suggested by Phil

--- examples/Case7_SOB_SRB.jl
+++ examples/Case7_SOB_SRB.jl
@@ -2,29 +2,14 @@ using Biofilm
 
 # Constants used for growthrates of particulate(s)
 KmB1 = 0.200; KmB3 = 11;  KmC2 = 20;  KI = 1;
-mumaxA = 0.4;  mumaxB = 0.672;  mumaxC = 1.46;
-
-## Define light as a function of time and depth within biofilm
-
-smoothHeaviside(t,t0)=tanh.(100*(t.-t0).-0.5)
-
-# Light :         turns on at t=0.25             turns off at t=0.75
-intensity(t) = smoothHeaviside(mod(t,1),0.25)-smoothHeaviside(mod(t,1),0.75)
-
-# Dissipation of light into biofilm (1 at top with a rate of decrease of diss)
-diss=2000;  # Dissipation rate into biofilm [1/m]
-dissipation(z,Lf) = max.(0.0,1.0.-(Lf.-z)*diss)
-
-# Light at a given location 
-light(t,z,Lf) = intensity(t)*dissipation(z,Lf)
+mumaxA = 0.4;  mumaxB = 6.72;  mumaxC = 1.46;
 
 # Define a structure to hold all the parameters
 p = param(
     # Growthrates for each particulate (constants defined above!)
-    mu=[(S, X, Lf, t, z, p) -> mumaxA*light(t,z,Lf),
-        (S, X, Lf, t, z, p) -> mumaxB*(S[1,:]./(KmB1.+S[1,:])).*(S[3,:]./(KmB3.+S[3,:])),
-        (S, X, Lf, t, z, p) -> mumaxC*(S[2,:]./(KmC2.+S[2,:])).*(1.0./(1.0.+S[1,:]/KI))],
-    discontinuityPeriod=0.25,  # Let solver know when discontinuities (changes in light) occur
+    mu=[(S, X, Lf, t, z, p) -> mumaxB*(S[1,:]./(KmB1.+S[1,:])).*(S[3,:]./(KmB3.+S[3,:])), # SOB
+        (S, X, Lf, t, z, p) -> mumaxC*(S[2,:]./(KmC2.+S[2,:])).*(1.0./(1.0.+S[1,:]/KI))], # SRB
+    #discontinuityPeriod=0.25,  # Let solver know when discontinuities (changes in light) occur
 
     # Source of particulates (constants defined above)
     src=[(S, X, p) -> 0.0,
@@ -42,9 +27,9 @@ p = param(
     plotPeriod=5,    # Time between plots [days] (make multiple of outPeriod!)
 
     # Simulation
-    Title="Ramsing et al. 1993 Test Case",
+    Title="SOB-SRB Test Case",
     SNames=["Oxygen","Sulfate","Hydrogen Sulfide"],
-    XNames=["Phototroph","Sulfide-Oxidizer","Sulfate-Reducer"],
+    XNames=["SOB - Sulfide-Oxidizer","SRB - Sulfate-Reducer"],
     #                           SOB                 SRB
     makePlots=true,
 
@@ -52,26 +37,25 @@ p = param(
     V=0.01,        # Volume of tank [m³]
     A=1,          # Surface area of biofilm [m²]
     Q=10,          # Flowrate through tank [m³/s]
-    Xo=[1.0,1.0,1.0],# Tank particulate initial condition(s)
+    Xo=[1.0e-6,1.0e-6],# Tank particulate initial condition(s)
     So=[8.6,48.0,0.0],    # Tank substrate initial condition(s)
     LL=2.0e-4,    # Boundary layer thickness [m]
 
     # Biofilm
     Nz=20,            # Number of grid points to represent biofilm
-    Pbo=[0.2/3,0.2/3,0.2/3],     # Biofilm particulates initial condition(s)
-    Sbo=[8.6,48.0,0.0],     # Biofilm substrates initial condition(s)
+    Pbo=[0.05,0.15],     # Biofilm particulates initial condition(s)
+    Sbo=[8.6,48.0,1e-5],     # Biofilm substrates initial condition(s)
     Lfo=5.0E-6,    # Biofilm initial thickness [m]
 
     # Substance Constants
     # Biomass yield coeffficient on substrate
     #     oxygen  sulfate  Hy. sulfide
-    Yxs=[ -0.52    0.0      0.0        # Phototropho produces oxygen
-           0.58    0.0      0.09       # SOB uses oxygen and sulfide
+    Yxs=[  0.58    0.0      0.09       # SOB uses oxygen and sulfide
            0.00    0.584   -1.645],    # SRB uses sulfate and produces sulfide
     Daq=[1.51e-4,8e-5,1.21e-4],    # Substrate diffusion through boundary layer
     De =[6.8e-5,4e-5,6.04e-5],     # Substrate diffusion through biofilm     
-    rho=[2.5e5,2.5e5,2.5e5],     # Particulate densities
-    Kdet=100.0,     # Particulates detachment coefficient
+    rho=[2.5e5,2.5e5],     # Particulate densities
+    Kdet=30.0,     # Particulates detachment coefficient
 
     # Tolerance
     tol=1e-4,
