91b9456deb62743727a5a0905a7b6be1cd17d22a
markowkes <mark.owkes@montana.edu>
2022-10-10 08:53:28 -0400

    Create Case7_SRB.jl
    Simplification of full SOB - SRB case

---
+++ examples/Case7_SRB.jl
@@ -0,0 +1,61 @@
+using Biofilm 
+
+# Constants used for growthrates of particulate(s)
+KmB1 = 0.200; KmB3 = 11;  KmC2 = 20;  KI = 1.0;
+mumaxA = 0.4;  mumaxB = 0.672;  mumaxC = 1.46;
+
+# Define a structure to hold all the parameters
+p = param(
+    # --------------------- #
+    # Simulation Parameters #
+    # --------------------- #
+    Title="SRB Test Case",
+    tFinal=500,     # Simulation time [days]
+    tol=1e-4,       # Tolerance
+    outPeriod=10,   # Time between outputs [days]
+    plotPeriod=20,  # Time between plots [days] (make multiple of outPeriod!)
+
+    # ---------------------- #
+    # Particulate Parameters #
+    # ---------------------- #
+    XNames=["SRB - Sulfate-Reducer"], # Particulate names
+    Xo=[1.0e-6],  # Tank particulate concentration initial condition(s)
+    Pbo=[0.2], # Biofilm particulates volume fraction initial condition(s) 
+    rho=[2.5e5], # Particulate densities
+    Kdet=50.0, # Particulates detachment coefficient
+    src=[(S, X, t, p) -> 0.0], # Source of particulates
+    # Growthrates for each particulate (constants defined above!)
+    mu=[(S, X, Lf, t, z, p) -> mumaxC*(S[2,:]./(KmC2.+S[2,:])).*(1.0./(1.0.+S[1,:]/KI))], #.-0.01*S[1,:]], # SRB
+
+    # -------------------- #
+    # Substrate Parameters #
+    # -------------------- #
+    SNames=["Oxygen","Sulfate","Hydrogen Sulfide"], # Substrate names
+    Sin=[(t) -> 8.6     # Substrate inflow (can be function of time)
+         (t) -> 48.0
+         (t) -> 0.0],
+    So=[8.6,48.0,0.0],   # Tank substrate concentration initial condition(s)
+    Sbo=[8.6,48.0,1e-5], # Biofilm substrates concentration initial condition(s)
+    # Biomass yield coefficient on substrate
+    #     oxygen  sulfate  Hy. sulfide
+    Yxs=[  0.00    0.584   -1.645],    # SRB uses sulfate and produces sulfide
+    Daq=[1.51e-4,8e-5,1.21e-4],    # Substrate diffusion through boundary layer
+    De =[6.8e-5,4e-5,6.04e-5],     # Substrate diffusion through biofilm     
+    
+    # --------------- #
+    # Tank Parameters #
+    # --------------- #
+    V=0.01,       # Volume of tank [m³]
+    A=1,          # Surface area of biofilm [m²]
+    Q=10,         # Flowrate through tank [m³/s]
+
+    # ------------------ #
+    # Biofilm Parameters #
+    # ------------------ #
+    Nz=20,          # Number of grid points in biofilm
+    Lfo=5.0e-6,     # Biofilm initial thickness [m]
+    LL=2.0e-4,      # Boundary layer thickness [m]
+)
+
+t,zm,X,S,Pb,Sb,Lf,sol = BiofilmSolver(p) # Run solver
+makePlots(t,X,S,Pb,Sb,Lf,p) # Plot final results
