fc498cce72a3b011578f6ca54e8c2af7de9d6cdb
Mark Owkes <mark.owkes@montana.edu>
2023-02-09 15:35:53 -0500

    Updated Case 4
    Added Dead particulate, updated some values

--- examples/Case4.jl
+++ examples/Case4.jl
@@ -2,33 +2,39 @@ using Biofilm
 
 # Constants used for growthrates of particulate(s)
 KmB1 = 0.200; KmB3 = 11;  KmC2 = 20;  KI = 1.0;
-mumaxA = 0.4;  mumaxB = 0.672;  mumaxC = 1.46;
-
+mumaxA = 0.4;  mumaxB = 0.672;  mumaxC = 10.46;
+D_O_SRB = 10.0
+D_SOB = 1e-2
+D_SRB = 1e-1
 # Define a structure to hold all the parameters
 p = param(
     # --------------------- #
     # Simulation Parameters #
     # --------------------- #
     Title="SOB-SRB Test Case",
-    tFinal=250,     # Simulation time [days]
+    tFinal=100,     # Simulation time [days]
     tol=1e-4,       # Tolerance
-    outPeriod=10,   # Time between outputs [days]
+    outPeriod=1,   # Time between outputs [days]
     plotPeriod=20,  # Time between plots [days] (make multiple of outPeriod!)
     #optionalPlot="source", # 6th plot: "growthrate" (default) or "source"
+    plotSize=(900,600), # Plot size [pixels]
+
     
     # ---------------------- #
     # Particulate Parameters #
     # ---------------------- #
-    XNames=["SOB - Sulfide-Oxidizer","SRB - Sulfate-Reducer"], # Particulate names
-    Xto=[1.0e-6,1.0e-6],  # Tank particulate concentration initial condition(s)
-    Pbo=[0.2/2,0.2/2], # Biofilm particulates volume fraction initial condition(s) 
-    rho=[2.5e5,2.5e5], # Particulate densities
-    Kdet=50.0, # Particulates detachment coefficient
-    srcX=[(S, X, t, p) -> 0.0, # Source of particulates
-         (S, X, t, p) -> -10*S[1,:]], # dies near oxygen
+    XNames=["SOB - Sulfide-Oxidizer","SRB - Sulfate-Reducer","Dead Bacteria"], # Particulate names
+    Xto=[1.0e-6,1.0e-6,0.0],  # Tank particulate concentration initial condition(s)
+    Pbo=[0.2/2,0.2/2,0.0], # Biofilm particulates volume fraction initial condition(s) 
+    rho=[2.5e5,2.5e5,2.5e5], # Particulate densities
+    Kdet=500.0, # Particulates detachment coefficient
+    srcX=[(S, X, t, p) -> - D_SOB*X[1,:]                                 ,  # SOB - slowly dies
+          (S, X, t, p) ->                - D_SRB*X[2,:] - D_O_SRB*S[1,:] ,  # SRB dies near oxygen and slowly dies
+          (S, X, t, p) -> + D_SRB*X[2,:] + D_SOB*X[1,:] + D_O_SRB*S[1,:] ], # Dead bacteria (opposite of above) 
     # Growthrates for each particulate (constants defined above!)
     mu=[(S, X, Lf, t, z, p) -> mumaxB*(S[1,:]./(KmB1.+S[1,:])).*(S[3,:]./(KmB3.+S[3,:])), # SOB
-        (S, X, Lf, t, z, p) -> mumaxC*(S[2,:]./(KmC2.+S[2,:])).*(1.0./(1.0.+S[1,:]/KI))], # SRB
+        (S, X, Lf, t, z, p) -> mumaxC*(S[2,:]./(KmC2.+S[2,:])).*(1.0./(1.0.+S[1,:]/KI)) , # SRB
+        (S, X, Lf, t, z, p) -> 0.0*S[1,:] ] , # Dead
 
     # -------------------- #
     # Substrate Parameters #
@@ -42,7 +48,8 @@ p = param(
     # Biomass yield coefficient on substrate
     #     oxygen  sulfate  Hy. sulfide
     Yxs=[  0.058    0.0      0.09       # SOB uses oxygen and sulfide
-           0.00    0.584   -1.645],    # SRB uses sulfate and produces sulfide
+           0.00    0.584   -1.645       # SRB uses sulfate and produces sulfide
+           0.00     0.0      0.0 ],     # Dead not needed/used for growth
     Daq=[1.51e-4,8e-5,1.21e-4],    # Substrate diffusion through boundary layer
     De =[6.8e-5,4e-5,6.04e-5],     # Substrate diffusion through biofilm     
     srcS=[(S,X,t,p) -> 0.0,
@@ -58,7 +65,7 @@ p = param(
     # ------------------ #
     # Biofilm Parameters #
     # ------------------ #
-    Nz=50,          # Number of grid points in biofilm
+    Nz=40,          # Number of grid points in biofilm
     Lfo=5.0e-6,     # Biofilm initial thickness [m]
     LL=2.0e-4,      # Boundary layer thickness [m]
 )
