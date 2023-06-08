091c6bdb2585b41ab6c67cb4aa6f9919883e4df6
markowkes <mark.owkes@montana.edu>
2023-06-08 10:18:00 -0400

    Updates to figures due to response to reviewers

--- HydrogenPeroxideDosing.jl
+++ HydrogenPeroxideDosing.jl
@@ -1,93 +1,90 @@
-using Biofilm 
-using OrdinaryDiffEq
-using Measures
-using Plots
-using Printf
-using Statistics
-using UnPack
-using JLD2
-
-# Create empty dictionary to hold parameters 
-d = createDict()
-
-smoothHeaviside(t,t0)=0.5*tanh.(10*(t.-t0).-0.5).+0.5
-
-# --------------------- #
-# Simulation Parameters #
-# --------------------- #
-addParam!(d, "Title",    "Hydrogen Peroxide Dosing")
-addParam!(d, "tFinal",  100)    # Simulation time [days]
-addParam!(d, "tol",      1e-8)  # Tolerance
-addParam!(d, "outPeriod",2.0)   # Time between outputs [days]
-addParam!(d, "plotPeriod",10)   # Time between plots [days] 
-#addParam!(d, "discontinuityPeriod",2.5) # Let solver know when discontinuities
-addParam!(d, "makePlots",false)
-
-# ---------------------- #
-# Particulate Parameters #
-# ---------------------- #
-addParam!(d, "XNames",["Live","Dead"])    # Particulate names
-addParam!(d, "Xto",   [1.0, 0.0])         # Tank particulate concentration initial condition(s)
-addParam!(d, "Pbo",   [0.08, 0.0])        # Biofilm particulates volume fraction initial condition(s) 
-addParam!(d, "rho",   [2.5e5, 2.5e5])     # Particulate densities
-addParam!(d, "Kdet",  1e4)                # Particulates detachment coefficient
-k_dis = 0.5; #m³/g/d   # Source term constant
-addParam!(d, "srcX",  [(S,X,Lf,t,z,p) -> -k_dis*X[1].*S[2], # Source of particulates
-                       (S,X,Lf,t,z,p) -> +k_dis*X[1].*S[2] ])
-# Growthrates for each particulate
-mumax = 9.6; # 1/days
-KM = 5; # g/m³
-addParam!(d, "mu", [(S,X,Lf,t,z,p) -> (mumax * S[1]) ./ (KM .+ S[1]) 
-                    (S,X,Lf,t,z,p) -> 0.0 ] )
-
-# -------------------- #
-# Substrate Parameters #
-# -------------------- #
-addParam!(d, "SNames",["Glucose","Hydrogen Peroxide"])     # Substrate names
-GlucoseIn = 100; #g/m³
-dose1 = 500.0; dose2 = 0.0;
-addParam!(d, "Sin",   [
-        (t) -> GlucoseIn,        # Substrate inflow (can be function of time)
-        (t) -> dose1*smoothHeaviside(t,2.0)+dose2*smoothHeaviside(t,6.0)])
-addParam!(d, "Sto",   [100.0, 0.0])          # Tank substrate concentration initial condition(s)
-addParam!(d, "Sbo",   [  0.0, 0.0])          # Biofilm substrates concentration initial condition(s)
-# Biomass yield coefficient on substrate
-addParam!(d, "Yxs",   [#Glucose   H. Per.
-                        0.26       0.0       # Live use glucose
-                        0.00       0.0   ])  # Dead doesn't use/produce anything
-addParam!(d, "Daq",   [5.2e-5, 1.09e-4])     # Substrate diffusion through boundary layer
-addParam!(d, "De",    [1.3e-5, 6.52e-5])     # Substrate diffusion through biofilm     
-k_bl  = 10.0; #m³/g/d
-k_bd  = 10.0; #m³/g/d
-addParam!(d, "srcS",  [                      # Source of substrates
-    (S,X,Lf,t,z,p) -> 0.0,  
-    (S,X,Lf,t,z,p) -> -k_bl*X[1].*S[2].-k_bd*X[2].*S[2] ])
-
-# --------------- #
-# Tank Parameters #
-# --------------- #
-addParam!(d, "V", 0.1)        # Volume of tank [m³]
-addParam!(d, "A",   1)        # Surface area of biofilm [m²]
-addParam!(d, "Q",   1)        # Flowrate through tank [m³/d]
-
-# ------------------ #
-# Biofilm Parameters #
-# ------------------ #
-addParam!(d, "Nz",  50)        # Number of grid points in biofilm
-addParam!(d, "Lfo", 50.0e-6)   # Biofilm initial thickness [m]
-addParam!(d, "LL",  1.0e-5)    # Boundary layer thickness [m]
-
-# Package and check parameters 
-p = packageCheckParam(d)
-
-"""
-Compute the % Live averaged throughout biofilm 
-"""
-function meanLive(Pb)
-    return mean(Pb[1,:])./(mean(Pb[1,:])+mean(Pb[2,:]))*100
+begin # Load packages 
+    using Biofilm 
+    using OrdinaryDiffEq
+    using Measures
+    using Plots
+    using Printf
+    using Statistics
+    using UnPack
+    using JLD2
+    using Accessors
 end
-function meanDead(Pb)
-    return mean(Pb[2,:])./(mean(Pb[1,:])+mean(Pb[2,:]))*100
+
+begin # Setup biofilm
+
+    # Create empty dictionary to hold parameters 
+    d = createDict()
+
+    smoothHeaviside(t,t0)=0.5*tanh.(10*(t.-t0).-0.5).+0.5
+
+    # --------------------- #
+    # Simulation Parameters #
+    # --------------------- #
+    addParam!(d, "Title",    "Hydrogen Peroxide Dosing")
+    addParam!(d, "tFinal",  100)    # Simulation time [days]
+    addParam!(d, "tol",      1e-8)  # Tolerance
+    addParam!(d, "outPeriod",10.0)   # Time between outputs [days]
+    addParam!(d, "plotPeriod",10)   # Time between plots [days] 
+    #addParam!(d, "discontinuityPeriod",2.5) # Let solver know when discontinuities
+    addParam!(d, "makePlots",false)
+
+    # ---------------------- #
+    # Particulate Parameters #
+    # ---------------------- #
+    addParam!(d, "XNames",["Live","Dead"])    # Particulate names
+    addParam!(d, "Xto",   [1.0, 0.0])         # Tank particulate concentration initial condition(s)
+    addParam!(d, "Pbo",   [0.08, 0.0])        # Biofilm particulates volume fraction initial condition(s) 
+    addParam!(d, "rho",   [2.5e5, 2.5e5])     # Particulate densities
+    addParam!(d, "Kdet",  1e4)                # Particulates detachment coefficient
+    k_dis = 0.5; #m³/g/d   # Source term constant
+    addParam!(d, "srcX",  [(S,X,Lf,t,z,p) -> -k_dis*X[1].*S[2], # Source of particulates
+                        (S,X,Lf,t,z,p) -> +k_dis*X[1].*S[2] ])
+    # Growthrates for each particulate
+    mumax = 9.6; # 1/days
+    KM = 5; # g/m³
+    addParam!(d, "mu", [(S,X,Lf,t,z,p) -> (mumax * S[1]) ./ (KM .+ S[1]) 
+                        (S,X,Lf,t,z,p) -> 0.0 ] )
+
+    # -------------------- #
+    # Substrate Parameters #
+    # -------------------- #
+    addParam!(d, "SNames",["Glucose","Hydrogen Peroxide"])     # Substrate names
+    GlucoseIn = 100; #g/m³
+    dose1 = 500.0; dose2 = 0.0;
+    addParam!(d, "Sin",   [
+            (t) -> GlucoseIn,        # Substrate inflow (can be function of time)
+            (t) -> dose1*smoothHeaviside(t,2.0)+dose2*smoothHeaviside(t,6.0)])
+    addParam!(d, "Sto",   [100.0, 0.0])          # Tank substrate concentration initial condition(s)
+    addParam!(d, "Sbo",   [  0.0, 0.0])          # Biofilm substrates concentration initial condition(s)
+    # Biomass yield coefficient on substrate
+    addParam!(d, "Yxs",   [#Glucose   H. Per.
+                            0.26       0.0       # Live use glucose
+                            0.00       0.0   ])  # Dead doesn't use/produce anything
+    addParam!(d, "Daq",   [5.2e-5, 1.09e-4])     # Substrate diffusion through boundary layer
+    addParam!(d, "De",    [1.3e-5, 6.52e-5])     # Substrate diffusion through biofilm     
+    k_bl  = 10.0; #m³/g/d
+    k_bd  = 10.0; #m³/g/d
+    addParam!(d, "srcS",  [                      # Source of substrates
+        (S,X,Lf,t,z,p) -> 0.0,  
+        (S,X,Lf,t,z,p) -> -k_bl*X[1].*S[2].-k_bd*X[2].*S[2] ])
+
+    # --------------- #
+    # Tank Parameters #
+    # --------------- #
+    addParam!(d, "V", 0.1)        # Volume of tank [m³]
+    addParam!(d, "A",   1)        # Surface area of biofilm [m²]
+    addParam!(d, "Q",   1)        # Flowrate through tank [m³/d]
+
+    # ------------------ #
+    # Biofilm Parameters #
+    # ------------------ #
+    addParam!(d, "Nz",  50)        # Number of grid points in biofilm
+    addParam!(d, "Lfo", 50.0e-6)   # Biofilm initial thickness [m]
+    addParam!(d, "LL",  1.0e-5)    # Boundary layer thickness [m]
+
+    # Package and check parameters 
+    p = packageCheckParam(d)
+
 end
 
 
@@ -99,19 +96,15 @@ begin # runs
     k_bl  = 10.0; #m³/g/d
     k_bd  = 10.0; #m³/g/d
     GlucoseIn = 100.0; # g/m³
-    dose1 =   0.0; dose2 =    0.0; t1,zm1,X1,S1,Pb1,Sb1,Lf1,sol1 = BiofilmSolver(p)
-    dose1 = 500.0; dose2 =    0.0; t2,zm2,X2,S2,Pb2,Sb2,Lf2,sol2 = BiofilmSolver(p)
-    dose1 = 500.0; dose2 = -500.0; t3,zm3,X3,S3,Pb3,Sb3,Lf3,sol3 = BiofilmSolver(p)
+    # Run out to 2000 days for Fig. 4a 
+    # (parameter studies will only run to 100 days)
+    p2000 = @set p.tFinal = 2000
+    dose1 =   0.0; dose2 =    0.0; t1,zm1,X1,S1,Pb1,Sb1,Lf1,sol1 = BiofilmSolver(p2000)
+    dose1 = 500.0; dose2 =    0.0; t2,zm2,X2,S2,Pb2,Sb2,Lf2,sol2 = BiofilmSolver(p2000)
+    dose1 = 500.0; dose2 = -500.0; t3,zm3,X3,S3,Pb3,Sb3,Lf3,sol3 = BiofilmSolver(p2000)
 end
 begin # plots
     fig = plot()
-    fig = plot!(t1,Lf1'.*1e6,linecolor = :black,line=(:solid,2), label="Dosing off")
-    fig = plot!(t2,Lf2'.*1e6,linecolor = :blue ,line=(:dash, 2), label="Dosing on")
-    fig = plot!(t3,Lf3'.*1e6,linecolor = :red  ,line=(:dot,  2), label="Dosing on/off")
-    fig = quiver!([2], [100], quiver=([0], [30]), linecolor = :black, line=(:solid, 1))
-    fig = annotate!([2],[90],"Dosing On")
-    fig = quiver!([6], [130], quiver=([0], [-30]), linecolor = :black, line=(:solid, 1))
-    fig = annotate!([6],[90],"Dosing Off")
     fig = plot!(
             xlabel = "Time (d)",
             ylabel = "Biofilm Thickness (μm)",
@@ -124,7 +117,15 @@ begin # plots
             ytickfontsize=14,
             legendfontsize=12,
             foreground_color_legend = nothing,
+            size=(650,400),
         )
+    fig = plot!(t1,Lf1'.*1e6,linecolor = :black,line=(:solid,2), label="No Dosing     ")
+    fig = plot!(t2,Lf2'.*1e6,linecolor = :blue ,line=(:dash, 2), label="Dosing on")
+    fig = quiver!([2], [100], quiver=([0], [30]), linecolor = :black, line=(:solid, 1))
+    fig = annotate!([2],[90],"Dosing On")
+    fig = plot!(t3,Lf3'.*1e6,linecolor = :red  ,line=(:dot,  2), label="Dosing on/off")
+    fig = quiver!([6], [130], quiver=([0], [-30]), linecolor = :black, line=(:solid, 1))
+    fig = annotate!([6],[90],"Dosing Off")
     display(fig)
     savefig("Fig1.svg")
     savefig("Fig1.pdf")
@@ -159,7 +160,7 @@ begin # runs
             global dose1 = doses[i]
             global dose2 = 0.0
             println("Running solver with dose1=$dose1")
-            t,zm,X,S,Pb,Sb,Lf,sol = BiofilmSolver(p)
+            local t,zm,X,S,Pb,Sb,Lf,sol = BiofilmSolver(p)
             push!( ts, t)
             push!(zms,collect(zm))
             push!( Xs, X)
@@ -214,6 +215,16 @@ begin # Plot Lf vs doses (add additional doses - make sure at S.S.)
 end
 
 begin # Plot Live vs doses (Average over biofilm)
+    """
+    Compute the % Live averaged throughout biofilm 
+    """
+    function meanLive(Pb)
+        return mean(Pb[1,:])./(mean(Pb[1,:])+mean(Pb[2,:]))*100
+    end
+    function meanDead(Pb)
+        return mean(Pb[2,:])./(mean(Pb[1,:])+mean(Pb[2,:]))*100
+    end
+
     fig = plot()
     fig = plot!(doses,map(i -> meanLive(Pbs[i]),1:length(doses)),
         linewidth = 2,  
@@ -283,10 +294,10 @@ begin # plot Glucose vs Depth in biofilm
     fig = plot()
     fig = plot!(vcat(zm1.*1e6,Lf1[end]*1e6),
                 vcat(Sb1[1,:],S_top1[1]),
-                line=(:solid, 2, :red),label="Dosing off")
+                line=(:solid, 2, :red),label="No Dosing")
     fig = plot!(vcat(zm2.*1e6,Lf2[end]*1e6),
                 vcat(Sb2[1,:],S_top2[1]),
-                line=(:dash, 2, :blue),label="Dosing on")
+                line=(:dash, 2, :blue),label="Dosing On")
     fig = plot!(
         xlabel = "Height in Biofilm (μm)",
         ylabel = "Glucose Concentration (g/m³)",
@@ -310,10 +321,10 @@ begin # plot Hydrogen Peroxide vs Depth in biofilm
     fig = plot()
     fig = plot!(vcat(zm1.*1e6,Lf1[end]*1e6),
                 vcat(Sb1[2,:],S_top1[2]),
-                line=(:solid, 2, :red),label="Dosing off")
+                line=(:solid, 2, :red),label="No Dosing")
     fig = plot!(vcat(zm2.*1e6,Lf2[end]*1e6),
                 vcat(Sb2[2,:],S_top2[2]),
-                line=(:dash, 2, :blue),label="Dosing on")
+                line=(:dash, 2, :blue),label="Dosing On")
                 #zm2.*1e6,Sb2[1,:],line=(:dash,  2, :blue),label="Dosing on")
     fig = plot!(
         xlabel = "Height in Biofilm (μm)",
@@ -341,10 +352,10 @@ end
 ## Uses results from runs in Fig. 1 ##
 begin # plot live & dead vs Depth in biofilm
     fig = plot()
-    fig = plot!(zm1.*1e6,Pb1[1,:],label="Live   - Dosing off",line=(:solid,2), linecolor = :red,)
-    fig = plot!(zm1.*1e6,Pb1[2,:],label="Dead - Dosing off",  line=(:dash, 2), linecolor = :red,)
-    fig = plot!(zm2.*1e6,Pb2[1,:],label="Live   - Dosing on ",line=(:solid,2), linecolor = :blue,)
-    fig = plot!(zm2.*1e6,Pb2[2,:],label="Dead - Dosing on ",  line=(:dash, 2), linecolor = :blue,)
+    fig = plot!(zm1.*1e6,Pb1[1,:],label="Live   - No Dosing",line=(:solid,2), linecolor = :red,)
+    fig = plot!(zm1.*1e6,Pb1[2,:],label="Dead -  No Dosing",  line=(:dash, 2), linecolor = :red,)
+    fig = plot!(zm2.*1e6,Pb2[1,:],label="Live   - Dosing On ",line=(:solid,2), linecolor = :blue,)
+    fig = plot!(zm2.*1e6,Pb2[2,:],label="Dead - Dosing On ",  line=(:dash, 2), linecolor = :blue,)
     fig = plot!(
         xlabel = "Height in Biofilm (μm)",
         ylabel = "Volume Fraction (-)",
@@ -387,8 +398,8 @@ begin # plot Glucose Consumption Rate vs Depth in biofilm
         Xb1[j,:] = p.rho[j]*Pb1[j,:]
         Xb2[j,:] = p.rho[j]*Pb2[j,:]
     end
-    fig = plot!(zm1.*1e6,consumption(Sb1,Pb1,Lf1,t1[end],zm1,p),line=(:solid, 2, :red), label="Dosing off")
-    fig = plot!(zm2.*1e6,consumption(Sb2,Pb2,Lf2,t2[end],zm2,p),line=(:dash,  2, :blue),label="Dosing on")
+    fig = plot!(zm1.*1e6,consumption(Sb1,Pb1,Lf1,t1[end],zm1,p),line=(:solid, 2, :red), label="No Dosing")
+    fig = plot!(zm2.*1e6,consumption(Sb2,Pb2,Lf2,t2[end],zm2,p),line=(:dash,  2, :blue),label="Dosing On")
     fig = plot!(
         xlabel = "Height in Biofilm (μm)",
         ylabel = "Glucose Consumption (g/m³-d)",
@@ -426,28 +437,27 @@ begin # run
 end
 begin # plot Biofilm Thickness vs Time
     fig = plot()
+    fig = plot!(
+        xlabel = "Time (d)",
+        ylabel = "Biofilm Thickness (μm)",
+        ylims = (0.0,200),
+        xlims = (0,10),
+        legend=:right,
+        xguidefontsize=16,
+        yguidefontsize=16,
+        xtickfontsize=14,
+        ytickfontsize=14,
+        legendfontsize=12,
+        foreground_color_legend = nothing,
+        size = (800,500),
+        margin = 10mm,
+    )
     fig = plot!(t2,Lf2'.*1e6,line=(:solid,2, :black), label="Live & Dead Neutralization")
     fig = plot!(t4,Lf4'.*1e6,line=(:dash, 2, :purple),label="Only Dead Neutralization")
     fig = plot!(t5,Lf5'.*1e6,line=(:dot,  2, :green), label="Only Live Neutralization")
-    #fig = quiver!([2], [70], quiver=([0], [70]), linecolor = :black, line=(:solid, 1))
-    #fig = annotate!([2],[60],"Dosing On")
     fig = quiver!([2], [20], quiver=([0], [40]), linecolor = :black, line=(:solid, 1))
     fig = annotate!([1.9],[40],text("Dosing On",:right,12))
-    fig = plot!(
-            xlabel = "Time (d)",
-            ylabel = "Biofilm Thickness (μm)",
-            ylims = (0.0,200),
-            xlims = (0,10),
-            legend=:right,
-            xguidefontsize=16,
-            yguidefontsize=16,
-            xtickfontsize=14,
-            ytickfontsize=14,
-            legendfontsize=12,
-            foreground_color_legend = nothing,
-            size = (800,500),
-            margin = 10mm,
-        )
+    
     display(fig)
     savefig("Fig5a.svg")
     savefig("Fig5a.pdf")
@@ -626,7 +636,7 @@ begin # runs (include lower concentrations - expect thickness to drop to zero at
         Lf_g = Matrix{Float64}[]
         for i in eachindex(GlucoseIns)
             global GlucoseIn = GlucoseIns[i]
-            t,zm,X,S,Pb,Sb,Lf,sol = BiofilmSolver(p)
+            local t,zm,X,S,Pb,Sb,Lf,sol = BiofilmSolver(p)
             push!( t_g, t)
             push!(zm_g,collect(zm))
             push!( X_g, X)
@@ -684,4 +694,3 @@ begin # Plot Live vs GlucoseIn (need depth averaged)
     savefig("Fig7b.svg")
     savefig("Fig7b.pdf")
 end
-
